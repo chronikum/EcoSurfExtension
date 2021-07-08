9c146d2be6c7e9d5938a9c364161b73fe6f427d8
Jonathan Fritz <fritz@nosc.io>
2021-07-08 10:15:20 +0200

    First working extension

--- contentScript.js
+++ contentScript.js
@@ -1,19 +1,77 @@
+
+const endpoint = "https://ecosurf.io/api/getValidation"
+
 let elements = document.getElementsByClassName("g");
 
+/**
+ * Thanks
+ * https://stackoverflow.com/a/23945027
+ */
+function extractHostname(url) {
+    var hostname;
+
+    if (url.indexOf("//") > -1) {
+        hostname = url.split('/')[2];
+    }
+    else {
+        hostname = url.split('/')[0];
+    }
+
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+    //find & remove "?"
+    hostname = hostname.split('?')[0];
+
+    return hostname;
+}
+
+async function makeAPICallForLink(link, element)
+{
+	link = extractHostname(link)
+	console.log("CALLING LINK: " + link)
+	var myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+
+	var requestOptions = {
+		method: 'POST',
+		mode: 'cors',
+		headers: myHeaders,
+		body: JSON.stringify({
+			"key": link
+		}),
+		redirect: 'follow'
+	};
+
+	const response = await fetch(endpoint, requestOptions).then(response => {
+		response.json().then(body => {
+			console.log(body)
+			if (body?.validation?.isGreen == 1)
+			{
+				element.style.backgroundColor = 'lightgreen';
+			}
+		})
+	});
+}
+
+/**
+ * Load validation for each link
+ */
+async function getInformationForLinkAndSetAtElement(link, element) {
+	makeAPICallForLink(link, element)
+}
 const allUrls = []
 
 for (var elementRef in elements)
 {
-	const element = elements[elementRef];
-
-	const link = element.querySelector(".g div div div a div cite")?.textContent;
-	if (link)
+	const element = elements[elementRef]
+	if (element)
 	{
-		allUrls.push(link)
-		console.log(link)
+		const link = element.querySelector(".g div div div a div cite")?.textContent;
+		if (link)
+		{
+			const onlyLink = link.split(" ")[0];
+			allUrls.push(onlyLink)
+			getInformationForLinkAndSetAtElement(onlyLink, element)
+		}
 	}
-}
-
-if (allUrls) {
-	console.log(allUrls)
 }
